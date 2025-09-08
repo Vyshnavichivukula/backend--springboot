8d16eeb8c15dbb4dc4d62ff03938915357fd11a4
Vyshnavichivukula <124250847+Vyshnavichivukula@users.noreply.github.com>
2025-09-08 16:04:01 +0530

    Update CartItem.java

--- src/main/java/com/example/ecommerce/model/src/main/java/com/example/ecommerce/model/src/main/java/com/example/ecommerce/model/CartItem.java
+++ src/main/java/com/example/ecommerce/model/src/main/java/com/example/ecommerce/model/src/main/java/com/example/ecommerce/model/CartItem.java
@@ -1,1 +1,18 @@
+package com.example.ecommerce.model;
+
+import javax.persistence.*;
+
+@Entity
+public class CartItem {
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Item item;
+
+    private int quantity;
+}
 
