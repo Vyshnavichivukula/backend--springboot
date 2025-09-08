19f064b2f5bce2efb66511166731f6386234a49d
Vyshnavichivukula <124250847+Vyshnavichivukula@users.noreply.github.com>
2025-09-08 16:15:26 +0530

    Create CartItem.java

---
+++ src/main/java/com/example/ecommerce/model/CartItem.java
@@ -0,0 +1,17 @@
+package com.example.ecommerce.model;
+
+import javax.persistence.*;
+
+@Entity
+public class CartItem {
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Item item;
+
+    private int quantity;
+}
